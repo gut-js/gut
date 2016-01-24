8804f50387dfa0f284a0a43c8e72e1ca93a427dd
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-23 17:43:54 -0800

    [setup] sets up basic redux tutorial

--- client/index.js
+++ client/index.js
@@ -1,6 +1,20 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Main from './components/Main.js';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import sayHello from './reducers/reducers'
+import App from './containers/App'
 
-render(<Main />,
-  document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(
+  thunk
+)(createStore)
+
+const store = createStoreWithMiddleware(sayHello)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
