af2d1fbf4c504debe45433dda7cd5baa02b8e2a8
Shin Adachi <hello@shinadachi.com>
2016-01-25 16:02:15 -0800

    [refactor] sets up routes.js, root URL goes to HomePage

---
+++ client/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import HomePage from './containers/HomePage';
+
+export default (
+	<Route path="/" component={HomePage} />
+);
