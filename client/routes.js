dea104e5be0350ca398bc0f5bf5477ccca2e3d52
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-26 16:42:01 -0800

    [setup] adds main.js container for rendering children routes

--- client/routes.js
+++ client/routes.js
@@ -2,24 +2,28 @@ import React from 'react';
 import { Router, Route, IndexRoute, browserHistory, hashHistory } from 'react-router';
 
 //Containers
+import Main from './containers/Main';
 import HomePage from './containers/HomePage';
 import Profile from './containers/Profile';
 
-// const checkAuth = (nextState, replace) => {
-// 	if(!localStorage.getItem('token')){
-// 		replace({
-// 			pathname: '/login'
-// 		})
-// 	}
-// }
+const checkAuth = (nextState, replace) => {
+	console.log(nextState);
+	// if(!localStorage.getItem('token')){
+	// 	replace({
+	// 		pathname: '/login'
+	// 	})
+	// }
+}
 
 // <IndexRoute component={Profile} onEnter={checkAuth} />
 // <Route path ='login' component={HomePage} />
 
 const routes = (
 	<Router history={hashHistory}>
-		<Route path='/' component={HomePage} />
+		<Route path='/' component={Main}>
+			<IndexRoute component={HomePage} onEnter={checkAuth} />
 			<Route path='profile' component={Profile} />
+		</Route>
 	</Router>
 );
 
