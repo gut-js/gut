623a2c7c2138a543e20ae92361424d47cd8fb56d
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-26 18:42:28 -0800

    [feat] maps state and passes to profile container

--- client/containers/Profile.js
+++ client/containers/Profile.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+//Actions
+import * as authActions from './../actions/authActions';
 
 // Containers
 import RestaurantList from './RestaurantList';
@@ -15,4 +20,19 @@ class Profile extends React.Component {
   }
 }
 
-export default Profile;
+function mapStateToProps(state){
+  return {
+    username: state.authReducer.username,
+    isLoggedIn: state.authReducer.isLoggedIn,
+    isFetching: state.authReducer.isFetching,
+    errorMessage: state.authReducer.errorMessage,
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    authActions: bindActionCreators(authActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
