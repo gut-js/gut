51acb5888cc8a08579bc22d74d4b27538c2bd1e6
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-28 11:22:10 -0800

    [feat] implements basic routing on profile to poll or menu

---
+++ client/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Menu extends React.Component {
+  render(){
+    return(
+      <div>
+        Hello from Menu!
+      </div>
+    )
+  }
+}
+
+export default Menu;
