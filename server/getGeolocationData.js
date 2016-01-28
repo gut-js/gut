57712a2a49ae05cc679324e6a828b28c152d3ca0
Shin Adachi <hello@shinadachi.com>
2016-01-28 13:58:00 -0800

    [setup] adds dependencies: q and request

---
+++ server/getGeolocationData.js
@@ -0,0 +1,22 @@
+var google_api_key = require('./config.js').google_api_key;
+var Q = require('q');
+var request = require('request');
+
+module.exports = function(){
+	var deferred = Q.defer();
+
+	request.post({url:'https://www.googleapis.com/geolocation/v1/geolocate?key='+google_api_key}, function(err, res, body){
+	  if(err){
+	    console.log("error:", err);
+	    deferred.reject("error within google geolocation POST request");
+	  }
+	  if(!err && res.statusCode === 200){
+	    deferred.resolve(JSON.parse(body))
+	  }
+	  else {
+	    deferred.reject("alt error");
+	  }
+	});
+
+	return deferred.promise;
+}
