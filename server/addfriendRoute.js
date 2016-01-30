74a21c4ae14589d53eb6debe183f8a6a171cbd4d
Shin Adachi <hello@shinadachi.com>
2016-01-29 21:09:30 -0800

    [pull] working on addfriend route

--- server/addfriendRoute.js
+++ server/addfriendRoute.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var db = require('./db');
+var addEdge = require('./addEdge');
 
 router.put('/',function(req,res){
 	console.log('inside addfriendRoute');
