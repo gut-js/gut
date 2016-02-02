5ea7ac6192a8ff5d2716a86789712debf818c4e8
Justin Tan <jtanman9@gmail.com>
2016-02-01 19:54:55 -0800

    [feat] adds Get Directions link to restaurant component

--- client/components/RestaurantPref.js
+++ client/components/RestaurantPref.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import magellan from 'magellan-coords';
 
 //Components
 import Map from './Map';
@@ -42,6 +43,38 @@ class RestaurantPref extends React.Component {
 
   displayTopRestaurant(){
     if (this.props.topRestaurant.name) {
+
+    // format start location for direction url
+    let destination = '';
+    this.props.topRestaurant.location.display_address.forEach(function (line, index, array) {
+      destination = destination.concat(line)
+      if (array[++index]) {
+        destination = destination.concat(' ')
+      }
+    })
+    destination = destination.replace(/\s/g, '+');
+
+    // convert coords
+    // TODO: remove hard-coded lat, lng
+    let startLat = '34.019383',
+        startLng = '-118.494491';
+
+    if (startLat.charAt(0) === '-') {
+      startLat = '-' + magellan(startLat.slice(1)).toDMS()
+    } else {
+      startLat = magellan(startLat).toDMS()
+    }
+
+    if (startLng.charAt(0) === '-') {
+      startLng = '-' + magellan(startLng.slice(1)).toDMS()
+    } else {
+      startLng = magellan(startLng).toDMS()
+    }
+
+    let startLoc = startLat + ',' + startLng;
+
+    let directionsUrl = 'https://www.google.com/maps/dir/' + startLoc + '/' + destination;
+
       return (
         <div>
           <h2>This is the best restaurant for you!</h2>
@@ -53,7 +86,9 @@ class RestaurantPref extends React.Component {
           <div style={{width:300, height:300}}>
             <Map lat={this.props.topRestaurant.location.coordinate.latitude} lng={this.props.topRestaurant.location.coordinate.longitude}/>
           </div>
-          <Button>Peas get directions!</Button>
+          <a href={directionsUrl} target='_blank'>
+            <Button>Peas get directions!</Button>
+          </a>
           <Button onClick={this.selectNext}>Next restaurant</Button>
         </div>
       )
