836c0d777e6d2a1299f6477d9ad8474bbfa26cca
Carl Bernardo <carlbernrdo@gmail.com>
2016-02-01 20:11:57 -0800

    [fix] displays query, removes whole list

--- client/components/FriendResult.js
+++ client/components/FriendResult.js
@@ -3,7 +3,7 @@ import React from 'react';
 class FriendResult extends React.Component {
   render(){
     const { username } = this.props;
-    console.log('name in results', username)
+
     return(
       <li className='list-group-item'>{username}</li>
     )
