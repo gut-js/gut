c15bba9f5f035134c243889f032ae21b61ccf623
Carl Bernardo <carlbernrdo@gmail.com>
2016-02-01 18:57:02 -0800

    [pull] saves progress for rebase

--- client/actions/friendActions.js
+++ client/actions/friendActions.js
@@ -7,20 +7,53 @@ export const REMOVE_REQUEST = 'REMOVE_REQUEST';
 export const REMOVE_SUCCESS = 'REMOVE_SUCCESS';
 
 // Main search function
-export const findFriends = (credentials) => {
-  // ajax call
+export const findFriends = (query) => {
+  // grab query and pass it in below
+  // pass in your own username as username
+  // pass in searchterm after searchterm
+
+  return dispatch => {
+    dispatch(searchRequest(query));
+
+    return fetch('http://localhost:5679/users?username=String&searchTerm=String', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      // fill this out
+    })
+    .catch(err => console.error('Error in Friend Search:', err));
+  }
 }
 
-const searchRequest = () => {
+const searchRequest = (query) => {
   // spinner when typing name in
+  return {
+    type: SEARCH_REQUEST,
+    query
+  }
 }
 
-const searchSuccess = () => {
+const searchSuccess = (searchResults) => {
   // return info from database and updates state
+  return {
+    type: SEARCH_SUCCESS,
+    searchResults
+  }
 }
 
-const searchError = () => {
+const searchError = (err) => {
   // if no users, trigger something on frontend
+  return {
+    type: SEARCH_ERROR,
+    err
+  }
 }
 
 // Main add friend function
@@ -30,10 +63,16 @@ export const addFriend = () => {
 
 const addRequest = () => {
   // triggers are you sure you want to add them
+  return {
+    type: ADD_REQUEST
+  }
 }
 
 const addSuccess = () => {
   // adds them, shows check mark
+  return {
+    type: ADD_SUCCESS
+  }
 }
 
 // Main remove friend Function
@@ -43,8 +82,14 @@ export const removeFriend = () => {
 
 const removeRequest = () => {
   // triggers are you sure you want to delete them
+  return {
+    type: REMOVE_REQUEST
+  }
 }
 
 const removeSuccess = () => {
   // removes them, shows them off your friends list
+  return {
+    type: REMOVE_SUCCESS
+  }
 }
