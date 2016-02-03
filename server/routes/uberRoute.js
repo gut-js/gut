129c73a745a5d4f1c4539e1fc9280a3fe7094436
Daisy Tsao <daisy.tsao@gmail.com>
2016-02-02 16:22:26 -0800

    [feat] gets real-time uber data for current location to recommended restaurant

--- server/routes/uberRoute.js
+++ server/routes/uberRoute.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 var request_uber = require('../functions/request_uber');
 router.get('/', function(req, res) {
-  request_uber('', function (error, response, body) {
+  console.log('req.query.coord: ', req.query.coord);
+  var coord = req.query.coord;
+  request_uber(coord, function (error, response, body) {
     if (error) {
       console.error(error);
     }
