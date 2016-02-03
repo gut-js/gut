479a762979ef49662a36696ee50a9f893aa56e20
Shin Adachi <hello@shinadachi.com>
2016-02-03 11:21:11 -0800

    [feat] server accepts POST request to /photo, saves uploaded file to uploads folder

--- server/routes/photoRoute.js
+++ server/routes/photoRoute.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 
-router.post('/', multer({dest:'../uploads/'}).single('userPhoto'),function(req,res,next){
+router.post('/', multer({dest:'./uploads/'}).single('userPhoto'),function(req,res,next){
+	
+	var tmp_path = req.file.path;
+
 	console.log('req.file',req.file);
 	res.status(204).end();
 });
