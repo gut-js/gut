4c196d4f99e1b333442f55cf3b8dea4fe30c7668
Carl Bernardo <carlbernrdo@gmail.com>
2016-02-04 15:56:08 -0800

    [refactor] refactors name of pref component

---
+++ client/components/RefinePref.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class RefinePref extends React.Component {
+  render(){
+    console.log('props in refine pref', this.props);
+    return(
+      <div>
+        hello from refinepref
+      </div>
+    )
+  }
+}
+
+export default RefinePref;
