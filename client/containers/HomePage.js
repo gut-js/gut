fdb74370b477c2cf6488fa3d7779d967f6a43eb4
Daisy Tsao <daisy.tsao@gmail.com>
2016-02-04 20:07:27 -0800

    [style] adds jumbotron with /signin and /register buttons

--- client/containers/HomePage.js
+++ client/containers/HomePage.js
@@ -8,12 +8,95 @@ import * as authActions from './../actions/authActions';
 
 //Components
 import Navigation from './../components/Navigation';
+import SignIn from './../components/SignIn';
+import Register from './../components/Register';
 
 class HomePage extends React.Component {
+  constructor(){
+    super();
+    this.openSignIn = this.openSignIn.bind(this);
+    this.closeSignIn = this.closeSignIn.bind(this);
+    this.openRegister = this.openRegister.bind(this);
+    this.closeRegister = this.closeRegister.bind(this);
+    this.state = {
+      showSignInModal: false,
+      showRegisterModal: false
+    }
+  }
+
+  openSignIn(){
+    this.setState({
+      showSignInModal: true
+    })
+  }
+
+  closeSignIn(){
+    this.setState({
+      showSignInModal: false
+    })
+  }
+
+  openRegister(){
+    this.setState({
+      showRegisterModal: true
+    })
+  }
+
+  closeRegister(){
+    this.setState({
+      showRegisterModal: false
+    })
+  }
+
   render(){
     return (
-      <div>
-        <Navigation {...this.props} />
+      <div className='home'>
+        <div id="bg-fade-carousel" className="carousel slide carousel-fade" data-ride="carousel">
+            <div className="carousel-inner">
+                <div className="item active">
+                    <div className="slide1"></div>
+                </div>
+                <div className="item">
+                    <div className="slide2"></div>
+                </div>
+                <div className="item">
+                    <div className="slide3"></div>
+                </div>
+                <div className="item">
+                    <div className="slide4"></div>
+                </div>
+                <div className="item">
+                    <div className="slide5"></div>
+                </div>
+            </div>
+            <div className="container carousel-overlay text-center">
+                <image height='100px' src="./../static/assets/snap_pea_logo.png"/>
+                <p className="lead" >Follow Your Gut</p>
+                <a 
+                  className="btn btn-lg btn-success fp-buttons"
+                  href="#" 
+                  onClick={this.openSignIn}>
+                  Sign In
+                  <SignIn
+                    {...this.props}
+                    showSignInModal={this.state.showSignInModal}
+                    closeSignIn={this.closeSignIn}
+                    openRegister={this.openRegister} />
+                </a>
+                <a 
+                  className="btn btn-lg btn-success fp-buttons"
+                  href="#"
+                  onClick={this.openRegister}>
+                  Register
+                  <Register
+                    {...this.props}
+                    showRegisterModal={this.state.showRegisterModal}
+                    closeRegister={this.closeRegister}
+                    openSignIn={this.openSignIn} />
+                </a>
+            </div>
+        </div>
+        
       </div>
     )
   }
