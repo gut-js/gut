bddcc5ea42007e0a5788b71503d73cf51f32b44f
Carl Bernardo <carlbernrdo@gmail.com>
2016-02-05 10:22:15 -0800

    [refactor] refactors redux poll

--- client/reducers/pollReducer.js
+++ client/reducers/pollReducer.js
@@ -1,15 +1,4 @@
-import {
-  LOAD_YELP_DATA,
-  SEND_POLL_REQUEST,
-  SEND_POLL_SUCCESS,
-  SEND_POLL_ERROR,
-  UPDATE_POLL,
-  SYNC_POLL,
-  END_POLL,
-  CLEAR_POLL,
-  RESET_REQUEST,
-  RESET_SUCCESS
-} from './../actions/pollActions';
+import * as ActionTypes from './../actions/pollActions';
 
 const initialState = {
   selected: [],
@@ -23,52 +12,52 @@ const initialState = {
 
 export default function pollReducer(state = initialState, action){
   switch(action.type){
-    case LOAD_YELP_DATA:
+    case ActionTypes.LOAD_YELP_DATA:
       return Object.assign({}, state, {
         data: action.info
       })
-    case SEND_POLL_REQUEST:
+    case ActionTypes.SEND_POLL_REQUEST:
       return Object.assign({}, state, {
         selected: action.info.selected,
         unselected: action.info.unselected,
         username: action.info.username,
       })
-    case SEND_POLL_SUCCESS:
+    case ActionTypes.SEND_POLL_SUCCESS:
       return Object.assign({}, state, {
         isSubmitting: false,
         pollErrorMessage: ''
       })
-    case SEND_POLL_ERROR:
+    case ActionTypes.SEND_POLL_ERROR:
       return Object.assign({}, state, {
         isSubmitting: false,
         pollErrorMessage: action.err
       })
-    case UPDATE_POLL:
+    case ActionTypes.UPDATE_POLL:
       return Object.assign({}, state, {
         data: action.results
       })
-    case SYNC_POLL:
+    case ActionTypes.SYNC_POLL:
       return Object.assign({}, state, {
         data: action.info,
         username: action.username
       })
-    case END_POLL:
+    case ActionTypes.END_POLL:
       return Object.assign({}, state, {
         isSubmitting: false,
         username: action.userInfo
       })
-    case CLEAR_POLL:
+    case ActionTypes.CLEAR_POLL:
       return Object.assign({}, state, {
         username: '',
         isSubmitting: true,
         pollErrorMessage: '',
         data: []
       })
-    case RESET_REQUEST:
+    case ActionTypes.RESET_REQUEST:
       return Object.assign({}, state, {
         resetCheck: false
       })
-    case RESET_SUCCESS:
+    case ActionTypes.RESET_SUCCESS:
       return Object.assign({}, state, {
         isSubmitting: true,
         pollErrorMessage: '',
