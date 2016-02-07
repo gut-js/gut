b6e1a95415b1c3db1b97a7fc1ab809a38895381e
Shin Adachi <hello@shinadachi.com>
2016-02-07 15:31:16 -0800

    [feat] poll route serve 100 businesses

--- server/routes/pollRoute.js
+++ server/routes/pollRoute.js
@@ -5,22 +5,48 @@ var _ = require('lodash');
 var request_yelp = require('../functions/request_yelp')
 
 router.get('/',function(req,res){
-	var city = _.shuffle(cities).pop();
-	request_yelp({location:city},function(yelpErr,yelpRes,yelpBody){
-		if (yelpErr){
-			console.log('error fetching yelp data');
-			res.json('error fetching yelp data');
-		}
-		var parsed = JSON.parse(yelpBody);
-		var businesses = parsed.businesses;
-        businesses = _.shuffle(businesses);
-        for (var i=0; i<businesses.length; i++) {
-          console.log(businesses[i].image_url);
-          businesses[i].image_url = businesses[i].image_url.slice(0,-6)+'o.jpg';
-        }
-        res.json(businesses);
-	})
-	//res.json(_.shuffle(businesses).slice(0,20));
+
+	var business100 = [];
+	// serving 100 businesses (alternate way)
+	cities = _.shuffle(cities);
+	for (var i=0; i<5; i++){
+		console.log('city: ',cities[i]);
+		request_yelp({location:cities[i]},function(yelpErr,yelpRes,yelpBody){
+			if (yelpErr){
+				console.log('error fetching yelp data');
+				res.json(yelpErr);
+			}
+			var parsed = JSON.parse(yelpBody);
+			var business20 = parsed.businesses;
+			for (var j=0; j<business20.length; j++){
+				business100.push(business20[j]);
+			}
+			if (business100.length>=100){
+				for (var k=0; k<business100.length; k++){
+					business100[k].image_url = business100[k].image_url.slice(0,-6)+'o.jpg';
+				}
+				res.json(_.shuffle(business100));
+			}
+		})
+	}
+
+
+	// // serving 20 businesses (our original way)
+	// var city = _.shuffle(cities).pop();
+	// request_yelp({location:city},function(yelpErr,yelpRes,yelpBody){
+	// 	if (yelpErr){
+	// 		console.log('error fetching yelp data');
+	// 		res.json('error fetching yelp data');
+	// 	}
+	// 	var parsed = JSON.parse(yelpBody);
+	// 	var businesses = parsed.businesses;
+ //        businesses = _.shuffle(businesses);
+ //        for (var i=0; i<businesses.length; i++) {
+ //          console.log(businesses[i].image_url);
+ //          businesses[i].image_url = businesses[i].image_url.slice(0,-6)+'o.jpg';
+ //        }
+ //        res.json(businesses);
+	// })
 })
 
 module.exports = router;
