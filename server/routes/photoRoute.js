ad59b2a77c33932f55d316b7ca30a0e87d1bb221
Daisy Tsao <daisy.tsao@gmail.com>
2016-02-13 11:43:57 -0800

    [fix] adds relative path to user profile pic

--- server/routes/photoRoute.js
+++ server/routes/photoRoute.js
@@ -14,7 +14,7 @@ router.post('/', multer({dest:'./client/static/assets/avatar/'}).single('image')
         console.log('err finding user');
       }
       else {
-        user.avatarUrl = "http://54.200.133.56:8080/static/assets/avatar/" + username;
+        user.avatarUrl = "./../static/assets/avatar/" + username;
         user.save(function(){})
       }
     })
